fa9d4988df283076e6c21893458af7de353e3e5a
SatoMichi <mangetumanjuu@gmail.com>
2020-05-15 20:01:19 +0100

    統一了numPai and charPai as one Class Pai

--- Pai.py
+++ Pai.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class numPai:
+class Pai:
     i = ["一","二","三","四","五","六","七","八","九"]
     s = ["萬","筒","索","中","發","白","東","西","南","北"]
     img = [['p_ms1_1.gif', 'p_ms2_1.gif', 'p_ms3_1.gif', 'p_ms4_1.gif',
@@ -12,12 +12,18 @@ class numPai:
             ['p_ji_c_1.gif', 'p_ji_h_1.gif', 'p_no_1.gif', 'p_ji_e_1.gif', 'p_ji_w_1.gif',
             'p_ji_s_1.gif', 'p_ji_n_1.gif']]
 
-    def __init__(self,num,suit):
-        self.num = num-1
-        self.suit = suit-1
+    def __init__(self,suit,num=-1):
+        # self.suit : (0,萬) (1,筒) (2,索) (3,中) (4,發) (5,白) (6,東) (7,西) (8,南) (9,北)
+        self.suit = suit
+        # self.num : (-1,字牌) (0..8, 数牌的数)
+        self.num = num
+        
 
     def __str__(self):
-        return self.i[self.num]+self.s[self.suit]
+        if self.num == -1:
+            return self.s[self.suit]
+        else:
+            return self.i[self.num]+self.s[self.suit]
 
     def __lt__(self, other):
         if self.suit != other.suit:
@@ -28,18 +34,31 @@ class numPai:
             return self.num < other.num
 
     def imgPathU(self):
-        path = self.img[self.suit][self.num]
+        if self.num == -1:
+            path = self.img[3][self.suit-3]
+        else:
+            path = self.img[self.suit][self.num]
         return "img/imgUp/" + path
     def imgPathD(self):
-        path = self.img[self.suit][self.num][:-5] +"2"+".gif"
+        if self.num == -1:
+            path = self.img[3][self.suit-3][:-5] +"2"+".gif"
+        else:
+            path = self.img[self.suit][self.num][:-5] +"2"+".gif"
         return "img/imgDown/" + path
     def imgPathL(self):
-        path = self.img[self.suit][self.num][:-5] +"3"+".gif"
+        if self.num == -1:
+            path = self.img[3][self.suit-3][:-5] +"3"+".gif"
+        else:
+            path = self.img[self.suit][self.num][:-5] +"3"+".gif"
         return "img/imgLeft/" + path
     def imgPathR(self):
-        path = self.img[self.suit][self.num][:-5] +"4"+".gif"
+        if self.num == -1:
+            path = self.img[3][self.suit-3][:-5] +"4"+".gif"
+        else:
+            path = self.img[self.suit][self.num][:-5] +"4"+".gif"
         return "img/imgRight/" + path
 
+"""
 class charPai(numPai):
     def __init__(self,suit):
         self.num = None
@@ -58,12 +77,12 @@ class charPai(numPai):
     def imgPathR(self):
         path = self.img[3][self.suit-3][:-5] +"4"+".gif"
         return "img/imgRight/" + path
+"""
 
-
-allPai = [numPai(num,suit) for num in range(1,10) for suit in range(1,4)] + [charPai(suit) for suit in range(4,11)]
+allPai = [Pai(suit,num) for suit in range(0,3) for num in range(0,9)] + [Pai(suit) for suit in range(3,10)]
 originalYama = np.random.permutation(allPai*4)
 
-Hand = [numPai]
+Hand = [Pai]
 
 def hand2Array(hand):
     ref = dict(zip(map(str,allPai),range(34)))
