e15af5a979d22311da08b155211fbea9e9ba67a0
SatoMichi <mangetumanjuu@gmail.com>
2020-06-18 20:02:47 +0100

    modified HeHand

--- HeHand.py
+++ HeHand.py
@@ -1,5 +1,3 @@
-import JudgeRon
-
 class HeHand:
     def __init__(self,beforeHand,hand,openHand,hepai,zifeng,changfeng,angang,tumo):
         self.beforeHand = beforeHand    # [[(int,int)]]: hand which is not Open and does not include hepai
@@ -17,13 +15,13 @@ class HeHand:
         return self.tumo
     # if 门前清 return True
     def isMenQing(self):
-        return self.openHand == [] or all(self.angang):
+        return self.openHand == [] or all(self.angang)
     # if 门前清 and Not 自摸 return True
     def isMenQingRon(self):
         return self.isMenQing() and not self.isTumo()
     # if 门前清自摸 return True
     def isMenQingTumo(self):
-        return self.isMenQing() and self.isTumo
+        return self.isMenQing() and self.isTumo()
     
     # retrun id of 雀头
     def queTou(self):
@@ -50,32 +48,33 @@ class HeHand:
 
     # helper function for Mianzi
     def judgeMianzi(self,paiset,open,angang):
-        if open and len(paiset)==3 and self.duanYaoJiu(paiset):
+        if open and len(paiset)==3 and self.YaoJiu(paiset):
+            return (paiset,"明刻幺九")
+        elif open and len(paiset)==3 and not self.YaoJiu(paiset):
             return (paiset,"明刻断幺九")
-        elif open and len(paiset)==3 and not self.duanYaoJiu(paiset):
-            return (paiset,"明刻2-8")
-        elif not open and len(paiset)==3 and self.duanYaoJiu(paiset):
+        elif not open and len(paiset)==3 and self.YaoJiu(paiset):
+            return (paiset,"暗刻幺九")
+        elif not open and len(paiset)==3 and not self.YaoJiu(paiset):
             return (paiset,"暗刻断幺九")
-        elif not open and len(paiset)==3 and not self.duanYaoJiu(paiset):
-            return (paiset,"暗刻2-8")
-        elif open and len(paiset)==4 and self.duanYaoJiu(paiset) and not angang:
+        elif open and len(paiset)==4 and self.YaoJiu(paiset) and not angang:
+            return (paiset,"明杠幺九")
+        elif open and len(paiset)==4 and not self.YaoJiu(paiset) and not angang:
             return (paiset,"明杠断幺九")
-        elif open and len(paiset)==4 and not self.duanYaoJiu(paiset) and not angang:
-            return (paiset,"明杠2-8")
-        elif open and len(paiset)==4 and self.duanYaoJiu(paiset) and angang:
+        elif open and len(paiset)==4 and self.YaoJiu(paiset) and angang:
+            return (paiset,"暗杠幺九")
+        elif open and len(paiset)==4 and not self.YaoJiu(paiset) and angang:
             return (paiset,"暗杠断幺九")
-        elif open and len(paiset)==4 and not self.duanYaoJiu(paiset) and angang:
-            return (paiset,"暗杠2-8")
         else:
             return (paiset,"顺子")
 
-    # helper function for judgeMianzi if paiset is 断幺九刻(杠)子 return True
-    def duanYaoJiu(self,paiset):
+    # helper function for judgeMianzi if paiset is 幺九刻(杠)子 return True
+    def YaoJiu(self,paiset):
         nums = [pai[0] for pai in paiset]
         isSame = len(set(nums)) == 1
         if isSame and nums[0] in [0,8,9,17,18,26,27,28,29,30,31,32,33]:
             return True
 
+    
     # return 等牌的型 (嵌張聴,辺張聴,单钓)
     def judgeWaiting(self):
         # waiting 雀头
@@ -84,7 +83,7 @@ class HeHand:
         # waiting paiset
         else:
             pentyan = [[0,1],[7,8],[9,10],[16,17],[18,19],[25,26]]
-            shunzi = self.takeSunzi([len(paiset) for paiset in self.beforeHand if len(paiset)==2])
+            shunzi = self.takeShunzi([paiset for paiset in self.beforeHand if len(paiset)==2])
             if shunzi == []:
                 return "双碰"
             elif [pai[0] for pai in shunzi[0]] in pentyan:
@@ -101,3 +100,46 @@ class HeHand:
             if paiset[0][0] == paiset[1][0]:
                 paisetsCopy.remove(paiset)
         return paisetsCopy
+
+    def __str__(self):
+        hand = []
+        for s in self.beforeHand:
+            for p in s:
+                hand.append(p)
+        ophand = []
+        for s in self.openHand:
+            for p in s:
+                ophand.append(p)
+        return Pai.showHand(hand) + Pai.showHand(ophand) + Pai.showHand([self.hepai])
+
+if __name__ == "__main__":
+    # test 1: 门前清栄和，雀头：中， 暗杠幺九，单钓
+    beforehand = [[(27,0)],[(1,0),(2,1),(3,2)],[(6,0),(7,1),(8,2)],[(22,0),(23,1),(24,2)]]
+    hepai = (27,1)
+    afterhand = [[(27,0),(27,1)],[(1,0),(2,1),(3,2)],[(6,0),(7,1),(8,2)],[(22,0),(23,1),(24,2)]]
+    ophand = [[(30,0),(30,1),(30,2),(30,3)]]
+    angang = [True]
+    zi = 30
+    chang= 30
+    tumo = False
+    hehand = HeHand(beforehand,afterhand,ophand,hepai,zi,chang,angang,tumo)
+    print("门前清自摸",hehand.isMenQingTumo())
+    print(hehand.Mianzi())
+    print(hehand.queTou())
+    print(hehand.judgeWaiting())
+    print("\n")
+
+    # test 2: 门前清栄和，雀头：中， 暗杠幺九，嵌張
+    beforehand = [[(27,0),(27,1)],[(1,0),(2,1),(3,2)],[(6,0),(7,1),(8,2)],[(22,0),(24,2)]]
+    hepai = (23,1)
+    afterhand = [[(27,0),(27,1)],[(1,0),(2,1),(3,2)],[(6,0),(7,1),(8,2)],[(22,0),(23,1),(24,2)]]
+    ophand = [[(30,0),(30,1),(30,2),(30,3)]]
+    angang = [True]
+    zi = 30
+    chang= 30
+    tumo = False
+    hehand = HeHand(beforehand,afterhand,ophand,hepai,zi,chang,angang,tumo)
+    print("门前清自摸",hehand.isMenQingTumo())
+    print(hehand.Mianzi())
+    print(hehand.queTou())
+    print(hehand.judgeWaiting())
