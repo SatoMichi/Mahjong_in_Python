3b645cd055c1aa9341e682b00d0082123b57f73b
SatoMichi <mangetumanjuu@gmail.com>
2020-07-25 15:25:39 +0100

    cretaed class for GUI expression

--- NewGUI/GameGUI.py
+++ NewGUI/GameGUI.py
@@ -8,10 +8,22 @@ class GameInfo:
     self.redbaopai = []
     self.playerInfos = []
 
-    def __init__(self,players):
-        self.gamecount = 
-
-
+    def __init__(self,GM):
+        self.GM = GM
+        self.gamecount = sum(GM.playerCounter)
+        self.playerTurn = GM.player.name
+        self.yama = GM.yama
+        self.baopai = GM.baopai
+        self.libaopai = GM.libaopai
+        self.redbaopai = GM.redbaopai
+        for p in GM.players:
+            self.playerInfos.append(PlayerInfo(p))
+    
+    def update(self):
+        self.__init__(self.GM)
+    
+    def draw(self):
+        pass
 
 
 class PlayerInfo:
@@ -35,5 +47,5 @@ class PlayerInfo:
         self.__init__(self.player)
 
     def draw(self):
-        
+        pass
 
